19334f9851eb13a100ce3e2ed4b522ed238c97c6
Sudarshan Shinde <sudarshan080498@gmail.com>
2023-11-20 13:29:46 +0530

    model Trainer

--- src/Components/data_ingestion.py
+++ src/Components/data_ingestion.py
@@ -1,12 +1,21 @@
 
 import os
 import sys
+
+from src.Components import data_transformation
+from src.Components import model_trainer
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.Components.data_transformation import DataTransformation
+from src.Components.data_transformation import DataTransformationConfig
+
+from src.Components.model_trainer import ModelTrainer
+from  src.Components.model_trainer import ModelTrainerConfig
 
 @dataclass
 class Dataingestionconfig:
@@ -15,10 +24,10 @@ class Dataingestionconfig:
     raw_data_path:str=os.path.join("artifacts","Data.csv")
 
 
-class Dataingestion:
+class DataIngestion:
     def __init__(self):
         self.ingestion_config = Dataingestionconfig()
-        
+
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
@@ -50,6 +59,12 @@ class Dataingestion:
         except Exception as e:
             raise CustomException(e,sys)
         
-if __name__=='__main__':
-    obj=Dataingestion()
-    obj.initiate_data_ingestion()
+if __name__== '__main__' :
+    obj=DataIngestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
